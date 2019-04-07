d0cfd56b26d6222d8c927467378919c7a592b785
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-07 20:27:57 +0300

    Create a custom UIButtonItem in the navigation bar

--- Simple_Browser/ViewController.swift
+++ Simple_Browser/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.google.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
